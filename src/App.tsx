7d195f568e196388171b4036d17dddd789d8b466
Pranav Gupta <pranavgupta@Pranavs-MacBook-Pro.local>
2022-08-21 21:38:18 -0400

    Implemented basic funtionality for the 'TodoForm' component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,15 @@ import Header from "./components/Header";
 import TodoForm from "./components/TodoForm";
 import "./App.css";
 
+type Todo = {
+  id: string;
+  text: string;
+  completed: boolean;
+};
+
 const App = () => {
   const [todoInput, setTodoInput] = useState("");
-  const [todoItems, setTodoItems] = useState([]);
+  const [todoItems, setTodoItems] = useState([] as Todo[]);
 
   return (
     <div className="app-container">
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,19 +1,48 @@
 import React, { FC } from "react";
+import { v4 as uuidV4 } from "uuid";
 
+type Todo = {
+  id: string;
+  text: string;
+  completed: boolean;
+};
 interface TodoFormProps {
   todoInput: string;
   setTodoInput: any;
-  todoItems: string[];
+  todoItems: Todo[];
   setTodoItems: any;
 }
 
-const TodoForm: FC<TodoFormProps> = () => {
+const TodoForm: FC<TodoFormProps> = ({
+  todoInput,
+  setTodoInput,
+  todoItems,
+  setTodoItems,
+}) => {
+  const onInputChange = (event: any) => {
+    setTodoInput(event.target.value);
+  };
+
+  const onFormSubmit = (event: any) => {
+    event.preventDefault();
+    setTodoItems((previousTodoItems: Todo[]) => {
+      return [
+        { id: uuidV4(), text: todoInput, completed: false } as Todo,
+        ...todoItems,
+      ];
+    });
+    setTodoInput("");
+  };
+
   return (
-    <form>
+    <form onSubmit={onFormSubmit}>
       <input
         type="text"
         placeholder="Whatchu gots to do bruv...?"
         className="todo-input"
+        value={todoInput}
+        required={true}
+        onChange={onInputChange}
       />
       <button className="button-add" type="submit">
         Add
